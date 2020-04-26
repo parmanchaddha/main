fe1beb3d35e7f51b9e52450a9a3899e2ef5e0f6e
ParmandeepChaddha <parmandeepchaddha@gmail.com>
2020-04-26 00:29:55 -0700

    Podcast changed.

--- pchaddha_website/src/Podcast.js
+++ pchaddha_website/src/Podcast.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-
+import Wobble from "react-reveal/Wobble";
 export default function Podcast () {
     console.log('hello world')
     return (
@@ -8,6 +8,14 @@ export default function Podcast () {
                 <div>
                     <h1> The Class in Session Podcast </h1>
                 </div>
+            </div>
+            <div className = "podcast-body">
+                <p className = "animated-paragraph">
+                    Recordings in progress 
+                    &nbsp;<span className="first">.</span>
+                    &nbsp;<span className="second">.</span>
+                    &nbsp;<span className="third">.</span> 
+                </p>
             </div>
         </div>
     )
