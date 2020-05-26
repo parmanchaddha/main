4e6145752c51c4f36d838a367bac183b5c824e43
Parmandeep Chaddah (acer) <pchaddha@uwaterloo.ca>
2020-05-26 01:32:37 -0400

    Styling for the navbar in Poems. Adding a new poem- the Speech.

--- pchaddha_website/src/Poems/poemDict.js
+++ pchaddha_website/src/Poems/poemDict.js
@@ -3,6 +3,7 @@ const busTitle = "The Bus";
 const menOfClothTitle = "The Men of the Cloth";
 const americaTheGreatTitle = "America The Great";
 const EPTitle = "Do I Know You?";
+const theSpeechTitle = "The Speech";
 
 
 
@@ -132,6 +133,26 @@ A day on which soul mates became extinct,
 And good enough became the accepted standard.`
 
 
+const theSpeechPoem = ` All of us, together. 1 mission, 1 chance.
+
+This will be the most important event in our lives.
+We fight for our freedom, our friends, our family. 
+We fight for those no longer. We fight for those yet to come.
+
+Believe in yourself. Trust your instincts. Stay sharp. Stay safe.  
+Pay attention of your sorroundings. This won't be easy.
+You may be pushed harder than you ever have been before. 
+Do not give up. I am counting on you. We all are.  
+
+Turn to your teammates now. You must trust them completely.
+For your sake. For their sake. Protect each other. Protect us. 
+Your team trusts you. Do not take that privelage lightly.
+We all trust you. Uphold that responsiblity.   
+
+Are you ready? You can do this. We will win. We have to.
+There's no other option...
+`;
+
 
 //Export PoemDict
 var poemDict = {
@@ -139,6 +160,7 @@ var poemDict = {
     "EP": {"title": EPTitle, "body": EPPoem },
     "americaTheGreat": {"title": americaTheGreatTitle, "body": americaTheGreatPoem },
     "menOfCloth": {"title": menOfClothTitle, "body": menOfClothPoem },
+    "theSpeech": {"title": theSpeechTitle, "body": theSpeechPoem }
 }
 
 export default poemDict;
