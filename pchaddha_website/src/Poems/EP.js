5c3211f0fceb5c1f8f29e354bc5d04bfec3b2482
Parmandeep Chaddah (acer) <pchaddha@uwaterloo.ca>
2020-05-26 21:55:36 -0400

    Separated out poems for easy adding. Fixed up the speech.

---
+++ pchaddha_website/src/Poems/EP.js
@@ -0,0 +1,29 @@
+//List All Poem Titles Here. 
+export const EPTitle = "Do I Know You?";
+
+//PoemBody
+export const EPPoem = `A woman made perfect by perception.
+Her quietness is resolution. 
+Her hesitation, thought.
+Her blue eyes are a summer breeze 
+Reminiscent of a care-free childhood. 
+
+She is always surrounded by friends,
+Shields guarding her innocence, 
+From the cruelty of the world. 
+
+One day, she tells me a secret. 
+
+---
+
+It became a day on which perfection died. 
+Not heroically flamed out in the battlefield protecting family, friends, and Love. 
+But by a mistake encouraged by impulse, and made potent by a bottle of Crown.
+
+A day on which a dirac delta, 
+Spiked at perfection, 
+Became a distribution, 
+Centered between personal best and worst. 
+
+A day on which soul mates became extinct, 
+And good enough became the accepted standard.`;
---
+++ pchaddha_website/src/Poems/menofcloth.js
@@ -0,0 +1,36 @@
+//List All Poem Titles Here. 
+export const menOfClothTitle = "The Men of the Cloth";
+
+//PoemBody
+export const menOfClothPoem = `We are resolute.
+Full of pride.
+‎Full of control.
+‎Full of wisdom.
+
+With hands behind our back,
+And our chest puffed out,
+We are the last- the last men.
+
+We are the men of the cloth.
+The men whose actions moved the heavens.
+The men whose actions moved hell.
+The men whose actions defined 'man'.
+
+But Time,
+That lass, whose ticking defines legacy-
+She promises the fruit of wisdom, whilst taking away the last of our strength.
+
+And so it is,
+These men of the cloth,
+Made weak by age,
+But strengthened in mind,
+Realize
+That love commands life.
+
+And
+
+Even these men,
+Who moved the Earth,
+Are helpless when their sons leave.
+
+And even they cry when mortality reminds them that they are not in control.`;
