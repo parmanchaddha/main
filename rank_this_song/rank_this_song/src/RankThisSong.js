55b5f38e77bbf14f004be8cc9ff09cd413f77b83
ParmandeepChaddha <parmandeepchaddha@gmail.com>
2020-07-06 22:41:36 -0700

    Initial commit for rank this song web app

--- rank_this_song/rank_this_song/src/RankThisSong.js
+++ rank_this_song/rank_this_song/src/RankThisSong.js
@@ -1,48 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import BandFormStepper from "./BandFormStepper";
 import './RankThisSong.css';
 
-async function getSongsByBand(band_name) {
-  let URL = `http://localhost:5000/get_songs_by_band?band_name=${band_name}/`;
-  let fetchURL = await fetch(URL);
-  let response = await fetchURL;
-  let results = await response.json();
-  let songs = await results.songs;
-  return songs
-}
-
-function BandNameInput (props) {
-  const [band_name, setBandName] = useState("");
-  const [songs, setSongs] = useState("");
-
-  async function fetchBand(search_band_name) {
-    let songs = await getSongsByBand(search_band_name);
-    setSongs(songs);
-  };
-
-  return (
-    <div className = {"band-name-input"}>
-      <form onSubmit={(event) => {event.preventDefault(); fetchBand(band_name)}}>
-        <input 
-          type = "text"
-          value = {band_name}
-          placeholder = {"Type the band name and press enter."}
-          onChange = {(event) => setBandName(event.target.value)}
-        />
-      </form>
-      {songs}
-    </div>
-  )
-}
-
 function RankThisSong() {
-  return (
+  return (  
     <div className="App">
       <header>
         <h1> Rank This Song</h1>
       </header>
 
       <div>
-        <BandNameInput />
+        <BandFormStepper />
       </div>
     </div>
   );
